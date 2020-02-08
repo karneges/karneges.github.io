f2a38df947e7029c9228c6bdbaa0b29bb604499c
Nikita <karneges2012@gmail.com>
2020-02-08 23:57:19 +0200

    add medium clone

---
+++ mediumclone/precache-manifest.7d7d252fbfae7f786422ab2f0095653b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f9becbac152568fcaf8e86b2f5f8d5",
+    "url": "/mediumclone/index.html"
+  },
+  {
+    "revision": "5d2e2f143e9b8685a309",
+    "url": "/mediumclone/static/css/main.06c3eb20.chunk.css"
+  },
+  {
+    "revision": "206685d5eb681bd71828",
+    "url": "/mediumclone/static/js/2.efb61906.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/mediumclone/static/js/2.efb61906.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d2e2f143e9b8685a309",
+    "url": "/mediumclone/static/js/main.b8e0d064.chunk.js"
+  },
+  {
+    "revision": "ecb0df0f0f709e79c4de",
+    "url": "/mediumclone/static/js/runtime-main.f1510aad.js"
+  }
+]);
