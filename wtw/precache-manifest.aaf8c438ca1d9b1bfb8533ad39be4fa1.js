31a51a858ee7ff43943efdaed7d9220d6a308543
Nikita <karneges2012@gmail.com>
2020-02-08 21:10:15 +0200

    add wtw

---
+++ wtw/precache-manifest.aaf8c438ca1d9b1bfb8533ad39be4fa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52542c571c5d458bbb5ca17da8e47161",
+    "url": "/karneges.github.io/wtw/index.html"
+  },
+  {
+    "revision": "6ef54247ae1ffb7f11c7",
+    "url": "/karneges.github.io/wtw/static/css/main.55608fe0.chunk.css"
+  },
+  {
+    "revision": "9aba799a072df34df7ce",
+    "url": "/karneges.github.io/wtw/static/js/2.caa86f62.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/karneges.github.io/wtw/static/js/2.caa86f62.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ef54247ae1ffb7f11c7",
+    "url": "/karneges.github.io/wtw/static/js/main.c13b5dd1.chunk.js"
+  },
+  {
+    "revision": "ff871e0a373c800db48e",
+    "url": "/karneges.github.io/wtw/static/js/runtime-main.16701a5f.js"
+  }
+]);
