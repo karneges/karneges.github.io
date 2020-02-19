ad806d71e352f03359ff65b59a392dbec1a24282
Nikita <karneges2012@gmail.com>
2020-02-19 20:46:01 +0200

    fix bugs list of articles

---
+++ listofarticles/precache-manifest.939ec6564587dbe6cccd431ddf016761.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b7cf7d0513e7797500058999b17bd3",
+    "url": "/karneges.github.io/listofarticles/index.html"
+  },
+  {
+    "revision": "e164ee4d2ec4adbd5b96",
+    "url": "/karneges.github.io/listofarticles/static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "d7ccb751a31145213368",
+    "url": "/karneges.github.io/listofarticles/static/css/main.2c00b9b4.chunk.css"
+  },
+  {
+    "revision": "e164ee4d2ec4adbd5b96",
+    "url": "/karneges.github.io/listofarticles/static/js/2.16c6c36e.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/karneges.github.io/listofarticles/static/js/2.16c6c36e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7ccb751a31145213368",
+    "url": "/karneges.github.io/listofarticles/static/js/main.444d08b5.chunk.js"
+  },
+  {
+    "revision": "9792601b1aab618687b4",
+    "url": "/karneges.github.io/listofarticles/static/js/runtime-main.76440df8.js"
+  }
+]);
